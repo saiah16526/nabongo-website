f32d9ab9111e957972f22b1ec6b45afba0c1d78a
saiah16526 <saiah.albert04@gmail.com>
2025-07-06 14:04:06 +0300

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import NabongoInstitute from "./pages/Nabongo-Institute";
 import NabongoTTC from "./pages/Nabongo-TTC";
 import NabongoJr from "./pages/Nabongo-Jr";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop /> {/* 👈 This forces the scroll to top */}
       <Routes>
         <Route path="/" element={<NabongoInstitute />} />
         <Route path="/ttc" element={<NabongoTTC />} />
